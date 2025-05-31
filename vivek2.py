26acba69fb2e9254d25702d14ffa2b4540145584
Vivek <saigoguladinne@gmail.com>
2025-05-31 09:55:48 +0530

    hi

--- vivek2.py
+++ vivek2.py
@@ -1,2 +1,20 @@
-a = "IIIIT KOTTAYAM"
-b = "MG"
+class chatbook:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.logged_in = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("Welcome to Chatbook! How would you proceed?\n " \
+        "1. Press to Signup\n" \
+        " 2. Press to Login\n" \
+        "3. Press 3 to write a post\n" \
+        "4. Press 4 to message a friend\n" \
+        "5. Press any other key to exit")
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
